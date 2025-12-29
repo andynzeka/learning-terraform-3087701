9e59c53575afed4c57df4ae5894ab5dc3954895d
Andy Nzeka <andynze4@gmail.com>
2025-12-29 12:36:42 -0600

    Variable-Test

--- main.tf
+++ main.tf
@@ -16,8 +16,9 @@ data "aws_ami" "app_ami" {
 
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
-  instance_type = "t3.nano"
-#  instance_type = "t2.micro"
+  # instance_type = "t3.nano"
+  # instance_type = "t2.micro"
+  instance_type = var.instance_type
 
   tags = {
     Name = "HelloWorld"
