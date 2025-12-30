22fd7d854bf5baba08cc26c44aa9a420a0efedc0
Andy Nzeka <andynze4@gmail.com>
2025-12-30 14:20:08 -0600

    module "alb"

--- main.tf
+++ main.tf
@@ -55,30 +55,31 @@ module "alb" {
   name    = "blog-alb"
   vpc_id  = module.blog_module-VPC.vpc_id
   subnets = module.blog_module-VPC.public_subnets
-
   security_groups = [module.blog_SG.security_group_id]
-  target_groups = {
-    ex-instance = {
+
+  target_groups = [
+    {
       name_prefix      = "blog"
-      protocol         = "HTTP"
-      port             = 80
+      backend_protocol = "HTTP"
+      backend_port     = 80
       target_type      = "instance"
-      target_id        = aws_instance.blog.id
-      health_check = {
-        path                = "/"
+      targets = {
+        my_target = {
+          target_id = aws_instance.blog.id
+          port      = 80
+        }
+      }
     }
-  }
-}
-  listeners = {
-    http = {
+  ]
+  listeners = [
+    {
       port               = 80
       protocol           = "HTTP"
       target_group_index = 0
     }
-  }
+  ]
   tags = {
-    Environment = "Development"
-    Project     = "Example"
+    Environment = "dev"
   }
 }
 
