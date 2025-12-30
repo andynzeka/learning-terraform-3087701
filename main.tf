49fa19ee464b6bcc953649a8996da6632fa3b5f8
Andy Nzeka <andynze4@gmail.com>
2025-12-30 12:26:22 -0600

    Use module for SG

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ resource "aws_instance" "blog" {
   # instance_type = "t2.micro"
   instance_type = var.instance_type
 
-  vpc_security_group_ids = [module.blog_module-SG.vpc_security_group_idss[0]]
+  vpc_security_group_ids = [module.blog_module-SG.vpc_security_group_ids[0]]
 
   tags = {
     Name = "Terraform Instance"
