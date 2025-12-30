f4833dcac824df96161918268062d827df135624
Andy Nzeka <andynze4@gmail.com>
2025-12-30 14:56:59 -0600

    module "autoscaling" fix

--- main.tf
+++ main.tf
@@ -48,7 +48,7 @@ module "autoscaling" {
 
   traffic_source_attachments = {
     alb = {
-      target_group_arn = module.blog_alb.target_group_arns
+      target_group_arn = module.blog_alb.target_group_arns["blog"]
     }
   }
 }
